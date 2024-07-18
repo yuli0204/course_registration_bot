04a1f53eb36703db7e02a5e78f74c901807270e3
yuli0204 <yuli0204@gmail.com>
2024-07-18 14:15:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def handle_track_registration(req):
         return {'fulfillmentText': 'You have no registered classes.'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
